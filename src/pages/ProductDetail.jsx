6408f991cfd59fd92d5685211a5559720d8b69f2
Lucasgcima <lucascimaglia@gmail.com>
2025-10-09 00:36:54 -0300

    Updates

--- src/pages/ProductDetail.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const ProductDetail = () => {
-  const { productId } = useParams();
-
-  return <h2>Detalle del producto con ID: {productId}</h2>;
-};
-
-export default ProductDetail;
--- src/pages/category.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const Category = () => {
-  const { categoryId } = useParams();
-
-  return <h2>Estás viendo la categoría: {categoryId}</h2>;
-};
-
-export default Category;
